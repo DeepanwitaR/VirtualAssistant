c843b6bb19e8d30b17847c41830c782b16f36d5e
DeepanwitaR <deepanwita23297@gmail.com>
2024-01-04 11:07:30 +0800

    Update virtualAssistant.py
    Final draft of function

--- virtualAssistant.py
+++ virtualAssistant.py
@@ -1,19 +1,31 @@
-# basic virtual asst program 
-
+# Virtual Assistant program
 from openai import OpenAI
+
 client = OpenAI()
 
-response = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "Deepa is from Singapore and works at Dell."},
-    {"role": "user", "content": "Where is Deepa from?"},
-    {"role": "user", "content": "What does Deepa do?"},
-    # {"role": "user", "content": "She is Deepa"},
-    {"role": "assistant", "content": "I know Deepa."},
-    # {"role": "user", "content": "Where was it played?"}
-  ]
-)
+def readFileFunc():
+  textFile = open("./information.txt", "r")
+  textData = textFile.read()
+  textFile.close()
 
-response_message = response.choices[0].message.content
-print(response_message )
+  return textData
+
+
+# Function to send questions to the ML model powered by Open API.
+def textGenerationAPIFunc(question):
+  textData = readFileFunc()
+  response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": textData}, # we have to provide context for every round of ML query (as a pretraining) since model does not retain information across session.
+      {"role": "user", "content": question},
+    ]
+  )
+  response_message = response.choices[0].message.content
+  print(response_message )
+
+
+print('Welcome! I am an OpenAI based virtual assistant. How may I assist you?')
+while 1:
+    question = input('\n')
+    textGenerationAPIFunc(question)
